36df6988b3ebc1bb36393722669d8e2bf82a8d86
Reanna Ishmael <rishmael@fordham.edu>
2024-02-10 14:42:51 -0500

    Add image generation endpoint to API

--- api/index.py
+++ api/index.py
@@ -1,3 +1,5 @@
+import requests
+
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -5,3 +7,15 @@ app = FastAPI()
 @app.get("/api/python")
 def hello_world():
     return {"message": "Hello World"}
+
+@app.get("/api/image")
+def get_image():
+    url = 'https://f1af-72-80-0-127.ngrok-free.app/sdapi/v1/txt2img'
+    payload = {
+        "prompt": "a detailed puppy dog",
+        "steps": 20,
+        "height": 1024,
+        "width": 1024
+    }
+    response = requests.post(url=url, json=payload)
+    return response.json()
