55d4ef6beab4837046c5dc45861443f0fad8208c
Reanna Ishmael <rishmael@fordham.edu>
2024-02-10 20:17:04 -0500

    Add Gemini text generation

--- app/page.js
+++ app/page.js
@@ -4,14 +4,18 @@ import React from 'react';
 import Image from 'next/image';
 
 import FearScreen from './components/FearScreen';
+import { GoogleGenerativeAI } from '@google/generative-ai';
 
 const ENDPOINT = '/api/image';
+const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GEMINI_API_KEY);
 
 export default function Home() {
-
   // initial || loading || success || error
   const [imageStatus, setImageStatus] = React.useState('initial');
+  const [storyStatus, setStoryStatus] = React.useState('initial');
+
   const [imageURI, setImageURI] = React.useState(null);
+  const [story, setStory] = React.useState('');
 
   async function handleSubmit(event) {
     event.preventDefault();
@@ -33,6 +37,28 @@ export default function Home() {
     }
   }
 
+  async function handleGemini(event) {
+    event.preventDefault();
+
+    setStoryStatus('loading');
+
+    try {
+      const model = genAI.getGenerativeModel({ model: 'gemini-pro' });
+
+      const prompt =
+        'Give me some facts to help me get over my fear of spiders.';
+
+      const result = await model.generateContent(prompt);
+      const response = await result.response;
+      const text = response.text();
+      setStory(text);
+
+      setStoryStatus('success');
+    } catch (err) {
+      setStoryStatus('error');
+    }
+  }
+
   return (
     <main>
       <FearScreen />
@@ -51,6 +77,12 @@ export default function Home() {
       ) : (
         'Nothing to see here...'
       )}
+      <form onSubmit={handleGemini}>
+        <button>Generate story</button>
+      </form>
+      {storyStatus}
+      <br />
+      {story}
     </main>
   );
 }
